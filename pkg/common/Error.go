442dac4367e66072294e4de772d360c68f889a72
wen <16117399@qq.com>
2023-02-07 15:08:10 +0000

    add accountIndex to errMsg

--- pkg/common/Error.go
+++ pkg/common/Error.go
@@ -1,13 +1,14 @@
 package common
 
 type ErrorMsg struct {
-	Exchange  ExchangeID `json:"exid"`
-	ActionID  ActionID   `json:"aid"`
-	Symbol    string     `json:"symbol"`
-	Id        string     `json:"id"`
-	Cid       string     `json:"cid"`
-	Side      SideID     `json:"side"`
-	Size      float64    `json:"size"`
-	Error     string     `json:"error"`
-	Timestamp int64      `json:"timestamp"`
+	Exchange     ExchangeID `json:"exid"`
+	AccountIndex AccountIdx `json:"accountidx"`
+	ActionID     ActionID   `json:"aid"`
+	Symbol       string     `json:"symbol"`
+	Id           string     `json:"id"`
+	Cid          string     `json:"cid"`
+	Side         SideID     `json:"side"`
+	Size         float64    `json:"size"`
+	Error        string     `json:"error"`
+	Timestamp    int64      `json:"timestamp"`
 }
--- pkg/common/Event.go
+++ pkg/common/Event.go
@@ -3,7 +3,7 @@ package common
 // 策略返回 统一数据格式
 type ActionEvent struct {
 	Exchange     ExchangeID    `json:"exid"`
-	AccountIndex AccountIdx    `json:"accountIdx"`
+	AccountIndex AccountIdx    `json:"accountidx"`
 	Transaction  TransactionID `json:"tid"`
 	Action       ActionID      `json:"aid"`
 	Data         interface{}   `json:"data"`
