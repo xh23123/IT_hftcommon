69e43aebe760731f00ea9605fceccd08b00fc8ed
wen <16117399@qq.com>
2023-03-30 10:20:04 +0000

    add exchange and transaction for order

--- pkg/common/Order.go
+++ pkg/common/Order.go
@@ -1,19 +1,21 @@
 package common
 
 type Order struct {
-	Symbol       string      `json:"symbol"`
-	Id           string      `json:"id"`
-	Cid          string      `json:"cid"`
-	Side         SideID      `json:"side"`
-	IsIsolated   bool        `json:"is_isolated"`
-	PositionSide PositionID  `json:"position_side"`
-	Type         OrderTypeID `json:"type"`
-	FilledSize   float64     `json:"filled_size"`
-	Size         float64     `json:"size"`
-	Price        float64     `json:"price"`
-	CreateTime   int64       `json:"create_time"`
-	CancelTime   int64       `json:"cancel_time"`
-	Status       StatusID    `json:"status"`
+	Exchange     ExchangeID    `json:"exid"`
+	Transaction  TransactionID `json:"tid"`
+	Symbol       string        `json:"symbol"`
+	Id           string        `json:"id"`
+	Cid          string        `json:"cid"`
+	Side         SideID        `json:"side"`
+	IsIsolated   bool          `json:"is_isolated"`
+	PositionSide PositionID    `json:"position_side"`
+	Type         OrderTypeID   `json:"type"`
+	FilledSize   float64       `json:"filled_size"`
+	Size         float64       `json:"size"`
+	Price        float64       `json:"price"`
+	CreateTime   int64         `json:"create_time"`
+	CancelTime   int64         `json:"cancel_time"`
+	Status       StatusID      `json:"status"`
 }
 
 type CancelInfo struct {
