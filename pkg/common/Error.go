76388c485749df5e21feb717b04bcbafe3967799
Ubuntu <ubuntu@ip-172-31-2-179.ap-northeast-1.compute.internal>
2023-11-08 16:48:02 +0800

    add gost error type for cancel failure

--- pkg/common/Error.go
+++ pkg/common/Error.go
@@ -1,15 +1,38 @@
 package common
 
-type ErrorMsg struct {
-	Exchange     ExchangeID    `json:"exid"`
-	Transaction  TransactionID `json:"tid"`
-	AccountIndex AccountIdx    `json:"accountidx"`
-	ActionID     ActionID      `json:"aid"`
-	Symbol       string        `json:"symbol"`
-	Id           string        `json:"id"`
-	Cid          string        `json:"cid"`
-	Side         SideID        `json:"side"`
-	Size         float64       `json:"size"`
-	Error        string        `json:"error"`
-	Timestamp    int64         `json:"timestamp"`
+import "fmt"
+
+var _ error = (*gostError)(nil)
+
+type gostError struct {
+	code int
+	msg  string
+}
+
+func (e gostError) Error() string {
+	return fmt.Sprintf("code:%d,msg:%v", e.code, e.msg)
+}
+
+func NewError(code int, msg string) error {
+	return gostError{
+		code: code,
+		msg:  msg,
+	}
+}
+
+func GetErrorCode(err error) int {
+	if e, ok := err.(gostError); ok {
+		return e.code
+	} else {
+		return ERRORCODE_WRONGTYPE
+	}
+
+}
+
+func GetErrorMsg(err error) string {
+	if e, ok := err.(gostError); ok {
+		return e.msg
+	} else {
+		return ""
+	}
 }
---
+++ pkg/common/ErrorCode.go
@@ -0,0 +1,5 @@
+package common
+
+const ERRORCODE_WRONGTYPE = -1             //not gostError
+const ERRORCODE_CANCEL_ORDER_NOT_EXIST = 1 //cancel failed due to no such order
+const ERRORCODE_CANCEL_REJECTED = 2        //cancel failed
