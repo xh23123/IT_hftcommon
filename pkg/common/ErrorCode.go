c3439268eb8d4c1c0a6fa3f53ef6bfc67cc9de9b
wen <ubuntu@ip-172-31-2-179.ap-northeast-1.compute.internal>
2023-12-01 15:37:13 +0800

    update

--- pkg/common/ErrorCode.go
+++ pkg/common/ErrorCode.go
@@ -4,3 +4,4 @@ const ERRORCODE_WRONGTYPE = -1             //not gostError
 const ERRORCODE_CANCEL_ORDER_NOT_EXIST = 1 //cancel failed due to no such order
 const ERRORCODE_CANCEL_REJECTED = 2        //cancel failed
 const ERRORCODE_ORDER_REJECTED = 3         //order failed
+const ERRORCODE_CANCEL_ALL_REJECTED = 4    //order not exist
