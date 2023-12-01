d99c7d486a3ff5e23bec5b66b18064473ffdc049
wen <ubuntu@ip-172-31-2-179.ap-northeast-1.compute.internal>
2023-12-01 15:08:57 +0800

    refactor ordermanager

--- pkg/common/OrderManagerInterface.go
+++ pkg/common/OrderManagerInterface.go
@@ -3,14 +3,14 @@ package common
 import cmap "github.com/orcaman/concurrent-map"
 
 type OrderManagerInterface interface {
+	TradingInterface
+
 	OpenOrder(orderId string) (*Order, bool)           //TODO
 	OpenOrdersBySymbol(symbol string) ([]*Order, bool) //TODO
 	AllOpenOrders() cmap.ConcurrentMap                 //TODO
 	CreateOrderProcess(event interface{}, handler func(data *Order) (id string, err error))
 	CancelOrderProcess(event interface{}, handler func(data CancelInfo) error)
 	CancelAllOrderProcess(event interface{}, handler func(data CancelInfo))
-	WsUpdateOrderOnOrder(OrderTradeUpdateInfo)
 	SetOpenOrder(orders []*Order)
-	WsUpdateOrderOnTrade(event OrderTradeUpdateInfo)
 	UpdateOpenOrder(openOrders []*Order) error
 }
