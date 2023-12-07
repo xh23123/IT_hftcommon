234910d225b4ff9199ed56192db6b7a111b7f258
wen <ubuntu@ip-172-31-2-179.ap-northeast-1.compute.internal>
2023-12-07 17:12:10 +0800

    print log for CheckOpenOrdersOnTime

--- pkg/common/OpenOrdersChecker.go
+++ pkg/common/OpenOrdersChecker.go
@@ -1,19 +1,36 @@
 package common
 
+import (
+	"go.uber.org/zap"
+)
+
+type orderChecker struct {
+	ExchangeId     ExchangeID
+	TransactionId  TransactionID
+	OrderCheckFunc func() error
+}
+
 type OpenOrderChecker struct {
-	needToRecheckOrderFunc []func() error
-	orderCheckFunc         []func() error
+	needToRecheckOrderFunc []orderChecker
+	orderCheckFunc         []orderChecker
 	lastFirstCheckTime     int64
 }
 
 const firstOrderCheckSecond = 54 // check at 54th second per minute
 const secondCheckInterval = 1
 
-func (b *OpenOrderChecker) AddOrderCheckFunc(checkFunc func() error) {
-	b.orderCheckFunc = append(b.orderCheckFunc, checkFunc)
+func (b *OpenOrderChecker) AddOrderCheckFunc(exchangeId ExchangeID, transactionId TransactionID, checkFunc func() error) {
+	if Logger == nil {
+		InitLogger("golog/common.log", "info")
+	}
+	b.orderCheckFunc = append(b.orderCheckFunc, orderChecker{
+		ExchangeId:     exchangeId,
+		TransactionId:  transactionId,
+		OrderCheckFunc: checkFunc,
+	})
 
 	if b.needToRecheckOrderFunc == nil {
-		b.needToRecheckOrderFunc = []func() error{}
+		b.needToRecheckOrderFunc = []orderChecker{}
 	}
 }
 
@@ -21,13 +38,30 @@ func (b *OpenOrderChecker) CheckOpenOrdersOnTime(curMilliSec int64) {
 	curSecond := curMilliSec / 1000
 	if b.firstCheckTime(curSecond) {
 		for _, v := range b.orderCheckFunc {
-			if err := v(); err != nil {
+			if err := v.OrderCheckFunc(); err != nil {
+				Logger.Error("OpenOrderChecker::CheckOpenOrdersOnTime failed firstCheckTime ",
+					zap.String("ExchangeId", string(v.ExchangeId)),
+					zap.String("TransactionId", string(v.TransactionId)),
+					zap.Error(err))
 				b.needToRecheckOrderFunc = append(b.needToRecheckOrderFunc, v)
+			} else {
+				Logger.Info("OpenOrderChecker::CheckOpenOrdersOnTime success firstCheckTime ",
+					zap.String("ExchangeId", string(v.ExchangeId)),
+					zap.String("TransactionId", string(v.TransactionId)))
 			}
 		}
 	} else if b.secondCheckTime(curSecond) {
 		for _, v := range b.needToRecheckOrderFunc {
-			v()
+			if err := v.OrderCheckFunc(); err != nil {
+				Logger.Error("OpenOrderChecker::CheckOpenOrdersOnTime failed secondCheckTime ",
+					zap.String("ExchangeId", string(v.ExchangeId)),
+					zap.String("TransactionId", string(v.TransactionId)),
+					zap.Error(err))
+			} else {
+				Logger.Info("OpenOrderChecker::CheckOpenOrdersOnTime success secondCheckTime ",
+					zap.String("ExchangeId", string(v.ExchangeId)),
+					zap.String("TransactionId", string(v.TransactionId)))
+			}
 		}
 		b.needToRecheckOrderFunc = b.needToRecheckOrderFunc[:0]
 	}
