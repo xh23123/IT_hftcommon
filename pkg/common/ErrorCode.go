4b1b0b9c2abd7f9048a1aecf8a13462c5d3e0939
wen <ubuntu@ip-172-31-38-34.ap-northeast-1.compute.internal>
2023-12-21 11:14:20 +0800

    add not enough balance

--- pkg/common/ErrorCode.go
+++ pkg/common/ErrorCode.go
@@ -10,7 +10,8 @@ const ERRORCODE_CANCEL_ALL_REJECTED ErrorCode = 4    //order not exist
 
 type ReasonCode int
 
-const REASON_UNKNOWN ReasonCode = 0       //unknown reason,should let developer know and fix it
-const REASON_LIMIT_BREACH ReasonCode = 1  //limit breach. strategy should stop and retry later
-const REASON_TIMEOUT ReasonCode = 2       //rest time out. strategy could retry
-const REASON_PARAM_INVALID ReasonCode = 3 //param invalid, e.g. price is 0,price exceed limit,symbol not exist, account not exist
+const REASON_UNKNOWN ReasonCode = 0           //unknown reason,should let developer know and fix it
+const REASON_LIMIT_BREACH ReasonCode = 1      //limit breach. strategy should stop and retry later
+const REASON_TIMEOUT ReasonCode = 2           //rest time out. strategy could retry
+const REASON_PARAM_INVALID ReasonCode = 3     //param invalid, e.g. price is 0,price exceed limit,symbol not exist, account not exist
+const REASON_NOTENOUGH_BALANCE ReasonCode = 4 // not enough balance, e.g. not enough balance to buy, not enough balance to pay fee, not enough margin
