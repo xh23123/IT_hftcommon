43a680a8669e19cbfbecced64d7efac43904b5e0
wen <ubuntu@ip-172-31-38-34.ap-northeast-1.compute.internal>
2024-01-08 15:53:56 +0800

    support binance plugin

--- pkg/common/BalanceManagerInterface.go
+++ pkg/common/BalanceManagerInterface.go
@@ -5,9 +5,11 @@ import cmap "github.com/orcaman/concurrent-map"
 type BalanceManagerInterface interface {
 	SetSpotBalance(balances cmap.ConcurrentMap)
 	SetFutureBalancePosition(balance cmap.ConcurrentMap, position cmap.ConcurrentMap)
+	SetCoinFutureBalancePosition(balance cmap.ConcurrentMap, position cmap.ConcurrentMap)
 	WsUpdateSpotBalance(balance SpotBalance)
 	GetSpotBalance(asset string) *Balance
 	GetFutureBalance(asset string) *Balance
+	GetCoinFutureBalance(asset string) *Balance
 	GetFuturePosition(symbol string, transactionId TransactionID) *FuturePosition
 	WsUpdateCoinFutureBalancePosition(balance WsFutureBalance, position WsFuturePosition)
 	WsUpdateFutureBalancePosition(balance WsFutureBalance, position WsFuturePosition)
