3cbae4e9b12544a51cee54bbda92619c8b951102
wen <ubuntu@ip-172-31-38-34.ap-northeast-1.compute.internal>
2024-01-17 11:28:42 +0800

    add

--- pkg/common/StrategyInterface.go
+++ pkg/common/StrategyInterface.go
@@ -1,28 +1,28 @@
 package common
 
 type OrderEntryCallback interface {
-	OnOrder(event *OrderTradeUpdateInfo) []ActionEvent
-	OnTrade(event *OrderTradeUpdateInfo) []ActionEvent
-	OnError(event *ErrorMsg) []ActionEvent
+	OnOrder(event *OrderTradeUpdateInfo) []*ActionEvent
+	OnTrade(event *OrderTradeUpdateInfo) []*ActionEvent
+	OnError(event *ErrorMsg) []*ActionEvent
 }
 
 type MarketDataCallback interface {
-	OnBookTick(event *BookTickWs) []ActionEvent
-	OnDepth(event *DepthWs) []ActionEvent
-	OnTick(event *TickWs) []ActionEvent
-	OnKlineWs(event *KlineWs) []ActionEvent
-	OnTradeWs(event *TradeWs) []ActionEvent
-	OnMarkPrice(event *MarkPriceWs) []ActionEvent
-	OnOrderbook(event *Orderbook) []ActionEvent
-	OnDexBookTicks(event *DexBookTicks) []ActionEvent
-	OnDexTrades(event *DexTrades) []ActionEvent
+	OnBookTick(event *BookTickWs) []*ActionEvent
+	OnDepth(event *DepthWs) []*ActionEvent
+	OnTick(event *TickWs) []*ActionEvent
+	OnKlineWs(event *KlineWs) []*ActionEvent
+	OnTradeWs(event *TradeWs) []*ActionEvent
+	OnMarkPrice(event *MarkPriceWs) []*ActionEvent
+	OnOrderbook(event *Orderbook) []*ActionEvent
+	OnDexBookTicks(event *DexBookTicks) []*ActionEvent
+	OnDexTrades(event *DexTrades) []*ActionEvent
 }
 
 type StrategyCommonFunctions interface {
 	InitPara()
 	InitVar()
-	InitMyStrategy() []ActionEvent
-	OnTimer() []ActionEvent
+	InitMyStrategy() []*ActionEvent
+	OnTimer() []*ActionEvent
 	OnExit()
 }
 
