a50b306f34afacdc8bea028a47b7de314ce666c3
wen <ubuntu@ip-172-31-47-59.ap-northeast-1.compute.internal>
2025-01-06 07:37:00 +0000

    support riskcontrol

--- pkg/common/TradeSystemInterface.go
+++ pkg/common/TradeSystemInterface.go
@@ -94,4 +94,5 @@ type GatewaySideInterface interface {
 type TradeSystemAgent interface {
 	StrategySideInterface
 	GatewaySideInterface
+	NotifyRisk(exid ExchangeID, accountIndex AccountIdx, transactionId TransactionID, reason string)
 }
